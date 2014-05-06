c81b3e2871c06045a83f49563a40d1f2f1311db7
Raitis Stengrevics <dagrevis@gmail.com>
2014-05-06 21:12:06 +0300

    Refactor evaluator

--- diylisp/evaluator.py
+++ diylisp/evaluator.py
@@ -19,60 +19,97 @@ def is_atom(ast):
     return not is_list(ast)
 
 
-def evaluate(ast, env):
-    """Evaluate an Abstract Syntax Tree in the specified environment."""
+def add(x, y):
+    return x + y
+
+
+def sub(x, y):
+    return x - y
+
+
+def mul(x, y):
+    return x * y
+
+
+def div(x, y):
+    return x / y
+
+
+def mod(x, y):
+    return x % y
+
+
+def gt(x, y):
+    return x > y
+
+
+def lt(x, y):
+    return x < y
+
+
+functions = {
+    "+": add,
+    "-": sub,
+    "*": mul,
+    "/": div,
+    "mod": mod,
+    ">": gt,
+    "<": lt,
+}
+
+
+def if_special(x, y, z):
+    if evaluate_wo_env(x):
+        return evaluate_wo_env(y)
+    return evaluate_wo_env(z)
+
+
+specials = {
+    "if": if_special,
+}
+
+
+def evaluate_wo_env(ast):
 
     # Handle quote.
     if is_list(ast) and ast[0] == "quote":
-        return evaluate(ast[1], env)
+        return evaluate_wo_env(ast[1])
 
     # Handle atom.
     if is_list(ast) and ast[0] == "atom":
-        ast = evaluate(ast[1], env)
+        ast = evaluate_wo_env(ast[1])
         return not is_list(ast)
 
     # Handle eq.
     if is_list(ast) and ast[0] == "eq":
-        x = evaluate(ast[1], env)
-        y = evaluate(ast[2], env)
+        x = evaluate_wo_env(ast[1])
+        y = evaluate_wo_env(ast[2])
         if not is_atom(x):
             return False
         return x == y
 
-    # Handle math.
-    math_operations = ("+", "-", "*", "/", "mod", ">", "<")
-    if is_list(ast) and ast[0] in math_operations:
+    # Handle functions.
+    if is_list(ast) and ast[0] in functions:
         operation = ast[0]
-        x = evaluate(ast[1], env)
-        y = evaluate(ast[2], env)
+        x = evaluate_wo_env(ast[1])
+        y = evaluate_wo_env(ast[2])
 
         if not is_integer(x) or not is_integer(y):
             raise LispError()
 
-        if operation == "+":
-            return x + y
-        if operation == "-":
-            return x - y
-        if operation == "*":
-            return x * y
-        if operation == "/":
-            return x / y
-        if operation == "mod":
-            return x % y
-        if operation == ">":
-            return x > y
-        if operation == "<":
-            return x < y
+        return functions[operation](x, y)
 
-    special_forms = ("if", )
-    if is_list(ast) and ast[0] in special_forms:
+    # Handle specials.
+    if is_list(ast) and ast[0] in specials:
         operation = ast[0]
+        args = ast[1:]
 
-        if operation == "if":
-            x = evaluate(ast[1], env)
-            if x:
-                return evaluate(ast[2], env)
-            else:
-                return evaluate(ast[3], env)
+        return specials[operation](*args)
 
     return ast
+
+
+def evaluate(ast, env):
+    """Evaluate an Abstract Syntax Tree in the specified environment."""
+
+    return evaluate_wo_env(ast)
